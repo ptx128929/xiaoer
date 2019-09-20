a0c3ec64a877d75b9679f31672068f7df03cf8e2
ptx128929 <2643007063@qq.com>
2019-09-20 09:06:38 +0800

    fg

--- Desktop/STS/pengtianxiang/src/main/java/com/pengtianxiang/utils/FileUtil.java
+++ Desktop/STS/pengtianxiang/src/main/java/com/pengtianxiang/utils/FileUtil.java
@@ -2,11 +2,13 @@ package com.pengtianxiang.utils;
 
 public class FileUtil {
 	/*
-	* 方法1：给定一个文件名，返回该文件名的扩展名，例如“aaa.jpg”，返回“.jpg”(10分)
-	*/
+	*   方法1  ：给定一  个文件名，返回该文件名的扩展名，例如“aaa.jpg”，返回“.jpg”(10分)
+	*/ 
 	public static String getExtendName(String fileName){
 		fileName.substring('.', 0);
-		return fileName;
+		return fileName;     
+		
+		
 	
 	}
 
